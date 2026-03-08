fb9f4f66d1dfb5faca172e31ac024e37d175ab02
Aksan Anan Ria <aksananan@iut-dhaka.edu>
2026-03-08 21:23:07 +0600

    input logic of study hours

--- src/user_reflection.py
+++ src/user_reflection.py
@@ -17,3 +17,16 @@ def input_study_date():
     except:
         print("Invalid date format. Using today.")
         return today_str()
+    
+
+def input_study_hours():
+    while True:
+        hours_str = input("How many hours did you study today? (1-24)\n> ").strip()
+        try:
+            hours = float(hours_str)
+            if 0 < hours <= 24:
+                return hours
+            else:
+                print("Please enter a valid number between 1 and 24.")
+        except:
+            print("Please enter a valid number between 1 and 24.")
